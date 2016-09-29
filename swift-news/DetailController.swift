80864fac7e0952775af728d4975de060f70c61c4
nabeen <watanabe_kenichiro@hasigo.co.jp>
2016-09-29 21:09:56 +0900

    読み込み処理作成

---
+++ swift-news/DetailController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailController.swift
+//  swift-news
+//
+//  Created by watanabe_kenichiro on 2016/09/29.
+//  Copyright © 2016年 nabeen. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+    
+}
---
+++ swift-news/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  swift-news
+//
+//  Created by watanabe_kenichiro on 2016/09/29.
+//  Copyright © 2016年 nabeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var entries = NSArray()
+    
+    let newsUrlString = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=https://catatsu.github.io/index.xml&num=10"
+    
+    @IBAction func refresh(sender: AnyObject) {
+        let url = NSURL(string: newsUrlString)
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let dataTask = session.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+            if let data = data where error == nil {
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as? NSDictionary
+                    let responseData = json!["responseData"] as? NSDictionary
+                    let feed = responseData!["feed"] as? NSDictionary
+                    let entries = feed!["entries"] as? NSArray
+                    self.entries = entries!
+                } catch _ as NSError {
+                    // エラー処理をする
+                }
+            }
+        })
+        dataTask.resume()
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableView.reloadData()
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("news")! as UITableViewCell
+        
+        let entry = entries[indexPath.row] as! NSDictionary
+        
+        cell.textLabel?.text = entry["title"] as? String
+        
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("detail", sender: entries[indexPath.row])
+    }
+}
+
